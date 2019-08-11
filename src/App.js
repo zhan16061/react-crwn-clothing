a69b764e9059df20eb21222b08c369d4ae294b9d
國展 <zhan16061@gmail.com>
2019-08-11 16:39:08 +0800

    add Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import './App.css'
 import Homepage from './Pages/Homepage/Homepage.jsx'
+import { Switch, Route } from 'react-router-dom'
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
 
 function App () {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   )
 }
