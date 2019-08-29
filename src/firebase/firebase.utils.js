3ab613fb77f7704fe1a9ede79c41697d8e3112a9
國展 <zhan16061@gmail.com>
2019-08-29 20:01:08 +0800

    added firebase to gitignore

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,16 +3,23 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 const config = {
+  apiKey: 'AIzaSyC2YqKNRNt7WkvaZ5fWtLcfHcrc54FrLb4',
+  authDomain: 'crwn-db-5b395.firebaseapp.com',
+  databaseURL: 'https://crwn-db-5b395.firebaseio.com',
+  projectId: 'crwn-db-5b395',
+  storageBucket: 'crwn-db-5b395.appspot.com',
+  messagingSenderId: '771376045836',
+  appId: '1:771376045836:web:dd3471e99f92106f'
 }
 export const createUser = async (userAuth, additionData) => {
-  if(!userAuth) return
+  if (!userAuth) return
   const userRef = firestore.doc(`users/${userAuth.uid}`)
   const userSnapShot = await userRef.get()
 
-  if(userSnapShot.exists) return userRef
+  if (userSnapShot.exists) return userRef
 
   const { displayName, email } = userAuth
-  const createAt = new Date();
+  const createAt = new Date()
 
   try {
     await userRef.set({
